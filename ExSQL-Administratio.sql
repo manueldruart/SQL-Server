fbfc62b4afdc7385bf665b2db061959521d7a6f2
manueldruart <manuel.druart@gmail.com>
2022-02-18 15:11:44 +0100

    add ex 2

--- ExSQL-Administratio.sql
+++ ExSQL-Administratio.sql
@@ -12,17 +12,34 @@ LOG ON
 (NAME = 'SQLServerDBTest_Log2', FILENAME='C:\Program Files\Microsoft SQL Server\MSSQL15.FORMATION\MSSQL\DATA\SQLServerDBTest_log2.ldf')
 
 USE SQLServerDBTest;
-
+GO
 CREATE SCHEMA Users
-
+GO
 CREATE TABLE Users.UserInfo
 ( id int , nom NVARCHAR(25));
-
+GO
 CREATE SCHEMA Systems
 CREATE TABLE Systems.SystemConfig
 ( id int , config NVARCHAR(25));
-
+GO
 CREATE TABLE OldOrders 
 ( id int , nom NVARCHAR(25), Date Datetime2) 
 ON Archive3;
  
+/*---------------------EX-2---------------------*/
+
+SELECT CASE SERVERPROPERTY ('IsIntegratedSecurtityOnly')
+WHEN 1 THEN 'WINDOWS AUTHENTIFICATION'
+WHEN 0 THEN 'Windows and SQL Server Authentification'
+END AS [Authentification MODE]
+
+USE master
+CREATE LOGIN Manu 
+WITH PASSWORD = 'Password' MUST_CHANGE, 
+CHECK_EXPIRATION = ON, CHECK_POLICY = ON,
+DEFAULT_DATABASE = SQLServerDBTest
+
+GRANT ALTER ANY DATABASE TO Manu
+
+/*---------------------EX-3---------------------*/
+
